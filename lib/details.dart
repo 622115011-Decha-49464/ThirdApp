90d03ee906a1722c26a65c9ac55f98b98fd7a96b
Decha_L <blackneko1234@hotmail.com>
2021-12-23 12:52:36 +0700

    Update Detail page

--- lib/details.dart
+++ lib/details.dart
@@ -1,6 +1,7 @@
-// ignore_for_file: prefer_const_constructors, avoid_unnecessary_containers
+// ignore_for_file: prefer_const_constructors, avoid_unnecessary_containers, empty_statements
 
 import 'package:flutter/material.dart';
+import 'package:thirdapp/main.dart';
 
 class DetailsPage extends StatefulWidget {
   const DetailsPage({Key? key}) : super(key: key);
@@ -12,8 +13,38 @@ class DetailsPage extends StatefulWidget {
 class _DetailsPageState extends State<DetailsPage> {
   @override
   Widget build(BuildContext context) {
-    return Container(
-      child: Text("Details Page"),
-    );
+    return Scaffold(
+        body: Row(
+      mainAxisAlignment: MainAxisAlignment.center,
+      children: [
+        Column(
+          children: [
+            Row(
+              children: [
+                IconButton(
+                    onPressed: () {
+                      Navigator.push(
+                          context,
+                          MaterialPageRoute(
+                              builder: (context) => const MyApp()));
+                    },
+                    icon: Icon(Icons.arrow_back_sharp)),
+                Text("Back to main page")
+              ],
+            ),
+            Divider(),
+            Text(
+              "Y E A H H H H H!!!!! Y E A H H H H H!!!!! Y E A H H H H H!!!!!",
+              style: TextStyle(
+                  color: Colors.orange,
+                  fontWeight: FontWeight.bold,
+                  fontSize: 30),
+            ),
+            Image.network(
+                "https://c.tenor.com/AUteKr8hSX0AAAAM/i-said-hey-he-man.gif"),
+          ],
+        )
+      ],
+    ));
   }
 }
